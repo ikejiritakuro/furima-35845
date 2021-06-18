7b4ee45c393d07aad2813a00e1a8ac9d32e502be
ikejiritakuro <talangchikao165@gmail.com>
2021-06-18 20:44:56 +0900

    テストコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
 
-with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-  validates :first_name  
-  validates :last_name
-end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name  
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
         
 
   validates :nickname, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
   validates :birth_date, presence: true  
 end
