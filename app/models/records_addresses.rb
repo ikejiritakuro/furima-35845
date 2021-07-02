2d4a2812c440a097596a3d03dc95994e02eb9588
ikejiritakuro <talangchikao165@gmail.com>
2021-07-02 10:41:30 +0900

    エラー文日本語実装完了

--- app/models/records_addresses.rb
+++ app/models/records_addresses.rb
@@ -8,10 +8,10 @@ class RecordsAddresses
     validates :token
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "半角数字で入力して下さい"}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "をハイフン(-)を入れて入力してください"}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "半角数字で入力してください"}
   end
-  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "は1以外の値にしてください"}
 
   def save
     record = Record.create(item_id: item_id, user_id: user_id)
